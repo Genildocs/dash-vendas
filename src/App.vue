87821504a4161b4fd3245fc5192c342801c0ec8c
genildocs <genildocs@gmail.com>
2025-03-29 05:21:42 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Camera } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div>
+    <h1>Dashboard de vendas</h1>
+    <span class="icon-avatar">AG</span>
+    <Camera />
+  </div>
+</template>
+
+<style scoped></style>
